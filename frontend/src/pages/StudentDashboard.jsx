1edb40e9eb6d64e91872f80a5bb96b85fb46c3a9
jp <2021t01213@stu.cmb.ac.lk>
2026-06-17 20:00:36 +0530

    update

--- frontend/src/pages/StudentDashboard.jsx
+++ frontend/src/pages/StudentDashboard.jsx
@@ -1,12 +1,11 @@
 import PortalLayout from "../layouts/PortalLayout";
 import UserOverview from "../components/userDashboard/UserOverview";
 import { studentTabs } from "../data/portalTabs";
-import { studentDashboardMockData } from "../data/studentDashboardData";
 
 const StudentDashboard = () => {
   return (
     <PortalLayout tabs={studentTabs} title="Student Portal">
-      <UserOverview data={studentDashboardMockData} />
+      <UserOverview />
     </PortalLayout>
   );
 };
