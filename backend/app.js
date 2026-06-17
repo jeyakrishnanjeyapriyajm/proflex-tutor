7a9cae8d8003a9df572363571b9dafb61937f1c7
jp <2021t01213@stu.cmb.ac.lk>
2026-06-17 16:54:25 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/task-giving", taskGivingRoutes);
 app.use("/api/admin", adminRoutes);
-
+app.use("/api/difficulty", difficultyRoutes);
 
 
 app.use("/api/model", modelRewardRoutes);
