580e6059aaa67c5b742f3a1b57debe2119fbb3dc
jp <2021t01213@stu.cmb.ac.lk>
2026-06-18 10:36:32 +0530

    update

--- frontend/src/services/adminAnalysisService.js
+++ frontend/src/services/adminAnalysisService.js
@@ -1,11 +1,11 @@
 import API from "./api";
 
-export const getAnalysisOverview = async () => {
+export const getAdminAnalysisOverview = async () => {
   const response = await API.get("/analysis/overview");
   return response.data;
 };
 
-export const getModuleAnalysis = async (moduleId) => {
+export const getAdminModuleAnalysis = async (moduleId) => {
   const response = await API.get(`/analysis/modules/${moduleId}`);
   return response.data;
 };
